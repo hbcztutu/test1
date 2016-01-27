1ddee03c9bca813527446125641da5e0320a60fb
unknown <>
2016-01-27 15:01:24 +0800

    Initial commit

---
+++ src/main/java/test.java
@@ -0,0 +1,13 @@
+/**
+ * Created by hbcztutu on 2016/1/27 0027.
+ */
+public class test {
+    public static String test() {
+        return "helloMaven";
+    }
+
+    public static void main(String[] args) {
+        test helloMaven = new test();
+        System.out.println(test.test());
+    }
+}
